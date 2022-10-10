63584036e101753b74c4fd054ef9a3d75f34bc66
Aleksey Kushnerov <kuschneroff.alex@yandex.ru>
2022-10-10 23:10:09 +0300

    Цикл for

---
+++ Cycle/Program.cs
@@ -0,0 +1,13 @@
+﻿// ВИД 4 Что-то принимают, что-то возвращают
+
+string Method4(int count, string text)
+{
+    string result = String.Empty;
+    for(int i = 0; i < count; i++)
+    {
+        result = result + text;
+    }
+    return result;
+}
+string res = Method4(10, "БРАВО>");
+Console.WriteLine(res);
