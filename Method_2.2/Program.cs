12094f200ec12895c831bb2853249869a440461e
Aleksey Kushnerov <kuschneroff.alex@yandex.ru>
2022-10-10 23:13:15 +0300

    Метод_вид_2.2

---
+++ Method_2.2/Program.cs
@@ -0,0 +1,11 @@
+﻿//ВИД 2.3 Что-то принимают, ничего не возвращают
+void Method21(string msg, int count)
+{
+   int i = 0;
+   while (i < count)
+   {
+    Console.WriteLine(msg);
+    i++;
+   }
+}
+Method21(count: 4, msg: "Новый текст");
