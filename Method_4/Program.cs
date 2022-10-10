3cc7ef61ab20409fa8ef2c0b3c040e9067d9bc75
Aleksey Kushnerov <kuschneroff.alex@yandex.ru>
2022-10-10 23:13:53 +0300

    Метод_вид_4

---
+++ Method_4/Program.cs
@@ -0,0 +1,16 @@
+﻿// ВИД 4 Что-то принимают, что-то возвращают
+
+string Method4(int count, string text)
+{
+    int i = 0;
+    string result = String.Empty;
+
+    while(i < count)
+    {
+        result = result + text;
+        i++; 
+    }
+    return result;
+}
+string res = Method4(10, "БРАВО>");
+Console.WriteLine(res);
